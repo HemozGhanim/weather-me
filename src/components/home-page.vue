c1b1925f1cb9df88a8bab8be0532bc87df7c2a8f
HemozGhanim <hemozghanim97@gmail.com>
2024-04-05 12:02:35 +0200

    almost finish

--- src/components/home-page.vue
+++ src/components/home-page.vue
@@ -21,7 +21,9 @@ const imageClasses = computed(() => {
 })
 onMounted(() => {
   if (Store.doneData) {
-    dataDone.value = true
+    setTimeout(() => {
+      dataDone.value = true
+    }, 500)
   } else {
     dataDone.value = false
   }
@@ -47,6 +49,16 @@ onUpdated(() => {
     v-if="!Store.doneData"
     class="flex items-center justify-center w-screen h-screen text-center text-white bg-indigo-900"
   >
+    <svg
+      class="animate-spin h-5 w-5 mr-2 iconsvg"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+    >
+      <path
+        fill="#ffffff"
+        d="M304 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 416a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM48 304a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z"
+      />
+    </svg>
     loading...
   </div>
   <div
@@ -148,7 +160,6 @@ onUpdated(() => {
     </div>
   </div>
 </template>
-
 <style scoped>
 .fade-enter-active,
 .fade-leave-active {
@@ -158,5 +169,8 @@ onUpdated(() => {
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
+}
+.iconsvg {
+  color: #ffffff !important;
 }
 </style>
